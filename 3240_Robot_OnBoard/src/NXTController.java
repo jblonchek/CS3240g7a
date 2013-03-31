eddd1925ca97fc691d414c4ea8b3ef8a266f91a7
Jonathon Blonchek <jrb2yk@virginia.edu>
2013-03-31 14:04:16 -0400

    Test.

--- 3240_Robot_OnBoard/src/NXTController.java
+++ 3240_Robot_OnBoard/src/NXTController.java
@@ -8,7 +8,7 @@ public class NXTController {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
-		Button.waitForAnyPress();
+		//Button.waitForAnyPress();
 
 	}
 
