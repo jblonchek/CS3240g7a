054a54b4fd74f1ead3558245a7ef447db93ad33e
Jonathon Blonchek <jrb2yk@virginia.edu>
2013-04-14 19:26:56 -0400

    Error Fixing of Mohammed Queue Work

--- 3240_Robot_OnBoard/src/CommThread.java
+++ 3240_Robot_OnBoard/src/CommThread.java
@@ -1,6 +1,8 @@
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.Queue;
 
 
 import lejos.nxt.Battery;
@@ -17,22 +19,27 @@ public class CommThread extends Thread {
 	Brick lejosBrick=new Brick();
 	DataInputStream dis;
 	DataOutputStream dos;
+	boolean expectAck = false;
 	
+	LinkedList<Packet> sendQueue = new LinkedList<Packet>(); //Queue<Packet>(); <- doesn't work, interface
 
-	
-	
+
+
 	@Override
 	public void run() {
 
-		String connected = "Connected";
-		String waiting = "Waiting...";
-		String closing = "Closing...";
-		int ackno = 0;
+		String connected 		= 		"Connected";
+		String waiting 			= 		"Waiting...";
+		String closing 			= 		"Closing...";
+		int ackno 				=		0;
+		long timeLastSent 		= 		0;
+		long timeLastRecieved 	= 		0;
 		Packet packetRecieved;
-		Message messageRecieved = null;;
-		Packet ackPacket = new Packet(Packet.PacketType.STATETYPE, new Message.NullMessage());
-		
-		
+		Packet packToSend 		= 		null;
+		Message messageRecieved =		null;;
+		Packet keepAlive;
+
+
 		LCD.drawString(waiting,0,0);
 		NXTConnection connection = Bluetooth.waitForConnection(); 
 		LCD.clear();
@@ -44,14 +51,40 @@ public class CommThread extends Thread {
 		while (!end) {
 
 			packetRecieved = Packet.deserialize(dis);
-		    messageRecieved = sendAckOrNack(packetRecieved);
 
-			//Message.NullMessage mess = new Message.NullMessage();
-			//Packet pack = new Packet(Packet.PacketType.STATETYPE, mess);
+			if(expectAck){ //based on Charles last message, we need to change what has expectAck=true, or remove this conditional statement (but keep effects)
+				if(packetRecieved == null &&  System.currentTimeMillis() - timeLastSent >= 750 || (packetRecieved.ackno < packToSend.seqno && (packetRecieved.seqno != Packet.EACKSEQNO && packetRecieved.msg.type != Message.MessageType.NULLMSG)) || (packetRecieved.seqno == Packet.ENACKSEQNO && packetRecieved.msg.type == Message.MessageType.NULLMSG)){
+					timeLastSent = System.currentTimeMillis();
+					try {
+						dos.write(packToSend.seralize());
+						dos.flush();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				} else {
+					expectAck = false;
+				}
+			} else if(!sendQueue.isEmpty()){
+				packToSend = (Packet) sendQueue.get(0);
+				sendQueue.remove(0);
+				try {
+					dos.write(packToSend.seralize());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				timeLastSent = System.currentTimeMillis();
+				expectAck = true;
+			}
 
 
 
 			if(packetRecieved != null) {
+				timeLastRecieved = System.currentTimeMillis();
+				if(packetRecieved.msg.type != Message.MessageType.NULLMSG && (packetRecieved.seqno != Packet.EACKSEQNO || packetRecieved.seqno != Packet.ENACKSEQNO)){
+					sendAckOrNack(packetRecieved);
+					ackno = packetRecieved.seqno;
+				}
+				messageRecieved = packetRecieved.msg;
 				if(messageRecieved.type == Message.MessageType.NULLMSG){
 //					LCD.clear();
 					LCD.drawString("NULL", 0, 0);
@@ -90,28 +123,54 @@ public class CommThread extends Thread {
 
 //					LCD.clear();
 					LCD.drawString(messageRecieved.toString(), 0, 0);
-
-					lejosBrick.clearQueue();
+					if (messageRecieved.pri.equals(Message.Priority.IMMEDIATE)){
+						lejosBrick.clearQueue();
+					} else {
+						lejosBrick.addToQueue(messageRecieved);
+					}
 				} else if(messageRecieved.type == Message.MessageType.GOTOS) {
+<<<<<<< HEAD
 //					LCD.clear();
+=======
+					
+					LCD.clear();
+>>>>>>> Error Fixing of Mohammed Queue Work
 					LCD.drawString(messageRecieved.toString(), 0, 0);
-				} else if(messageRecieved.type == Message.MessageType.ITER) {
-//					LCD.clear();
-					LCD.drawString(messageRecieved.toString(), 0, 0);
-				} else if(messageRecieved.type == Message.MessageType.PAUSEQ) {
-//					LCD.clear();
-					LCD.drawString(messageRecieved.toString(), 0, 0);
+					lejosBrick.addToQueue(messageRecieved);
 
-					lejosBrick.pauseQueue();
+				
+				} else if(messageRecieved.type == Message.MessageType.ITER) {
+<<<<<<< HEAD
+//					LCD.clear();
+=======
+				
+					LCD.clear();
+>>>>>>> Error Fixing of Mohammed Queue Work
+					LCD.drawString(messageRecieved.toString(), 0, 0);
+					lejosBrick.addToQueue(messageRecieved);
+
+			
+				} else if(messageRecieved.type == Message.MessageType.PAUSEQ) {
+<<<<<<< HEAD
+//					LCD.clear();
+=======
+				
+					LCD.clear();
+>>>>>>> Error Fixing of Mohammed Queue Work
+					LCD.drawString(messageRecieved.toString(), 0, 0);
+					if (messageRecieved.pri.equals(Message.Priority.IMMEDIATE)){
+						lejosBrick.pauseQueue();
+					} else {
+						lejosBrick.addToQueue(messageRecieved);
+					}
 				} else if(messageRecieved.type == Message.MessageType.READS) {
 //					LCD.clear();
 					LCD.drawString(messageRecieved.toString(), 0, 0);
 
 					if (messageRecieved.pri.equals(Message.Priority.IMMEDIATE)){
 						Message.ReadsMessage readSensorMessage=(Message.ReadsMessage) messageRecieved;
-						
-						respondToReadSensor(readSensorMessage);
-						
+						enqueuePacketToSend(makeReadSensorPacket(readSensorMessage));
+
 					} else {
 						lejosBrick.addToQueue(messageRecieved);
 					}
@@ -121,9 +180,8 @@ public class CommThread extends Thread {
 					LCD.drawString(messageRecieved.toString(), 0, 0);
 					if(messageRecieved.pri.equals(Message.Priority.IMMEDIATE)) {
 						Message.ReadrMessage readRobotMessage = (Message.ReadrMessage) messageRecieved;
+						enqueuePacketToSend(makeReadRoboPacket(readRobotMessage));
 
-						respondToReadRobo(readRobotMessage);
-						
 					} else { 
 						lejosBrick.addToQueue(messageRecieved);
 					}
@@ -136,12 +194,25 @@ public class CommThread extends Thread {
 					lejosBrick.stepQueue();
 				} 
 
+			} else if(System.currentTimeMillis() - timeLastRecieved >= 250){
+				keepAlive = new Packet(Packet.PacketType.COMMANDTYPE, new Message.NullMessage(), Packet.EACKSEQNO, ackno);		    	  
+				LCD.clear();
+				LCD.drawString("HI" + ackno, 0, 0);
+				try {	
+					dos.write(keepAlive.seralize());
+					dos.flush();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 			}
 		}
 	}
+
+	public void enqueuePacketToSend(Packet p){
+		sendQueue.add(p);
+	}
 	
-	
-	public void respondToReadRobo(Message.ReadrMessage readRobotMessage){
+	public Packet makeReadRoboPacket(Message.ReadrMessage readRobotMessage){
 		double resp = 0;
 		if(readRobotMessage.param == RoboData.BatteryStrength) {
 			resp = Battery.getVoltage();
@@ -158,32 +229,31 @@ public class CommThread extends Thread {
 		} else if(readRobotMessage.param == RoboData.Speed)	{
 			resp = RoboData.getSpeed();//(Motor.A.getSpeed() + Motor.B.getSpeed() + Motor.C.getSpeed());
 		}
-		
+
 		Message.ReadrMessage data = new Message.ReadrMessage(readRobotMessage.param, resp);
+		data.pri = Message.Priority.IMMEDIATE;
 		Packet dataPack = new Packet(Packet.PacketType.COMMANDTYPE, data);
-		try {
-			dos.write(dataPack.seralize());
-			dos.flush();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+
+		return dataPack;
+
 	}
-	
-	public void respondToReadSensor(Message.ReadsMessage readSensorMessage){
+
+	public Packet makeReadSensorPacket(Message.ReadsMessage readSensorMessage){
 		SensorData s = null;
 		if(readSensorMessage.s == Sensor.SENSORCS){
-			s = new ColorSensorClass();
+			s = new ColorSensorClass(); //We need a dummy value initialized somewhere for this. We can't actually connect a color sensor to the robot even if we have one, so while comm protocol dictates we must be able to respond with updates, we can't do anything meaningful
 		} else if(readSensorMessage.s == Sensor.SENSORLS) {
-			s = new LightSensorClass();
+			//s = new LightSensorClass();
 		} else if(readSensorMessage.s == Sensor.SENSORSS) {
-			s = new SoundSensorClass();
+			//s = new SoundSensorClass();
 		} else if(readSensorMessage.s == Sensor.SENSORTS) {
-			s = new TouchSensorClass();
+			//s = new TouchSensorClass();
 		} else if(readSensorMessage.s == Sensor.SENSORUS) {
-			s = new UltrasonicSensorClass();
+			//s = new UltrasonicSensorClass();
 		}
-		
+
 		Message.ReadsMessage data = new Message.ReadsMessage(readSensorMessage.s, s.update());
+<<<<<<< HEAD
 		data.pri=Message.Priority.IMMEDIATE;
 		Packet dataPack = new Packet(Packet.PacketType.COMMANDTYPE, data);
 		
@@ -224,28 +294,43 @@ public class CommThread extends Thread {
 	      }
 	      
 	      return messageRecieved;
+=======
+		data.pri = Message.Priority.IMMEDIATE;
+		Packet dataPack = new Packet(Packet.PacketType.COMMANDTYPE, data);
+
+		return dataPack;
+
+>>>>>>> Error Fixing of Mohammed Queue Work
 	}
-	
-	private boolean recieveAckOrNack(Packet pack){
-		
-		try {
-			Thread.sleep(250);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+
+	private void sendAckOrNack(Packet packetRecieved){
+		int ackno = 0;
+		Packet ackPacket = new Packet(Packet.PacketType.STATETYPE, new Message.NullMessage());
+		if(packetRecieved != null){
+			ackno = packetRecieved.seqno;
+			ackPacket = new Packet(Packet.PacketType.COMMANDTYPE, new Message.NullMessage(), Packet.EACKSEQNO, ackno);
+			LCD.clear();
+			LCD.drawString("HI" + ackno, 0, 0);
+			try {	
+				dos.write(ackPacket.seralize());
+				dos.flush();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} else {
+			ackPacket = new Packet(Packet.PacketType.COMMANDTYPE, new Message.NullMessage(), Packet.ENACKSEQNO, ackno);
+			LCD.clear();
+			LCD.drawString("BYE" + ackno, 0, 0);
+			try {	
+				dos.write(ackPacket.seralize());
+				dos.flush();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
-		Packet packetRecieved = Packet.deserialize(dis);
-		
-	    if(packetRecieved == null){
-	    	return false;
-	    } else {
-	    	if(packetRecieved.msg.type == Message.MessageType.NULLMSG && packetRecieved.ackno == pack.seqno && packetRecieved.seqno == 0){
-	    		return true;
-	    	} else {
-	    		return false;
-	    	}
-	    }
 
 	}
-	
+
+
+
 }
---
+++ 3240_Robot_OnBoard/src/CommandQueue.java
@@ -0,0 +1,174 @@
+
+public class CommandQueue extends Thread {
+
+	// TO DO:
+	// ITER- done??
+	// GOTOS- done??
+	// ABORTQ- done
+	// STEPQ-
+	// PAUSEQ- done
+	// STARTQ- done??
+	// CLEARQ- done
+	// ENQUEUE- done
+	// reads
+	// readr
+	// move
+	// turn
+
+	private Message[] queue;
+	private byte PC, queueEnd;
+	private boolean paused;
+	private CommThread comm;
+
+	public CommandQueue(CommThread comm) {
+		queue = new Message[100];
+		queueEnd = 0;
+		PC = 0;
+		paused = true;
+		this.comm = comm;
+	}
+
+	public void run() {
+		while (!(paused || this.interrupted())) {
+			stepQ();
+		}
+	}
+
+	public void enqueue(Message m) {
+
+		queue[queueEnd] = m;
+		queueEnd++;
+
+	}
+
+	public void startQ() {
+		paused = false;
+		this.start();
+	}
+
+	public void stepQ() {
+
+		Message curInst = queue[PC];
+		if (curInst.type == Message.MessageType.MOVE) {
+			comm.lejosBrick.move(0, ((Message.MoveMessage) curInst).dist, 0);
+		} else if (curInst.type == Message.MessageType.TURN) {
+			comm.lejosBrick.turn(((Message.TurnMessage) curInst).angle);
+		} else if (curInst.type == Message.MessageType.GOTOS) {
+			gotos();
+		} else if (curInst.type == Message.MessageType.ITER) {
+			iterQ();
+		} else if (curInst.type == Message.MessageType.PAUSEQ) {
+			pauseQ();
+		} else if (curInst.type == Message.MessageType.READS) {
+			comm.enqueuePacketToSend(comm
+					.makeReadSensorPacket((Message.ReadsMessage) curInst));
+		} else if (curInst.type == Message.MessageType.READR) {
+			comm.enqueuePacketToSend(comm
+					.makeReadRoboPacket((Message.ReadrMessage) curInst));
+		}
+
+		PC++;
+
+	}
+
+	public void pauseQ() {
+		paused = true;
+		try {
+			this.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		this.interrupt();
+	}
+
+	public void abortQ() {
+		pauseQ();
+		clearQ();
+
+	}
+
+	public void clearQ() {
+		queue = new Message[100];
+		PC = 0;
+		queueEnd = 0;
+		paused = true;
+	}
+
+	public void iterQ() {
+		Message.IterMessage m = (Message.IterMessage) queue[PC];
+		int curIterInst = PC;
+		for (int i = 0; i < m.count; i++) {
+			PC = (byte) m.destination;
+			while (PC < curIterInst) {
+				stepQ();
+			}
+		}
+
+	}
+
+	public void gotos() {
+		Message.GotosMessage m = (Message.GotosMessage) queue[PC];
+
+		double sdata = getData(m.sensor);
+		double cmpVal = 0;
+		if (m.compareMode == Message.GotosMessage.Condition.LT) {
+
+			cmpVal = (sdata < m.threshold + m.epsilon) ? 1 : 0;
+			// t > d
+		} else if (m.compareMode == Message.GotosMessage.Condition.LTE) {
+
+			cmpVal = (sdata <= m.threshold + m.epsilon) ? 1 : 0;
+
+			// t >= d
+		} else if (m.compareMode == Message.GotosMessage.Condition.EQ) {
+
+			cmpVal = (sdata < m.threshold + m.epsilon && sdata > m.threshold
+					- m.epsilon) ? 1 : 0;
+
+			// t = d
+		} else if (m.compareMode == Message.GotosMessage.Condition.GTE) {
+
+			cmpVal = (sdata >= m.threshold - m.epsilon) ? 1 : 0;
+
+			// t <= d
+		} else if (m.compareMode == Message.GotosMessage.Condition.GT) {
+
+			cmpVal = (sdata > m.threshold - m.epsilon) ? 1 : 0;
+
+			// t < d
+		} else if (m.compareMode == Message.GotosMessage.Condition.NE) {
+
+			cmpVal = (sdata > m.threshold + m.epsilon || sdata < m.threshold
+					- m.epsilon) ? 1 : 0;
+
+			// t != d
+		}
+
+		if (cmpVal == 1) {
+			PC = (byte) m.destination;
+		}
+
+	}
+
+	private double getData(Sensor s) {
+
+		SensorData sdata = null;
+		if (s == Sensor.SENSORCS) {
+			sdata = new ColorSensorClass(); // need a dummy value in the
+											// ColorSensorClass we use;									// explained in detail in comment in
+											// CommThread2
+		} else if (s == Sensor.SENSORLS) {
+			//sdata = new LightSensorClass();
+		} else if (s == Sensor.SENSORSS) {
+			//sdata = new SoundSensorClass();
+		} else if (s == Sensor.SENSORTS) {
+			//sdata = new TouchSensorClass();
+		} else if (s == Sensor.SENSORUS) {
+			sdata = new UltrasonicSensorClass();
+		}
+
+		return sdata.update();
+
+	}
+}
